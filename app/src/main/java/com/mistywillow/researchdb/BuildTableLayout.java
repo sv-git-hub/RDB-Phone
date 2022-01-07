8c4dfc569f5e9d24a359b452d5ab263bfc74b141
sv-git-hub <scottvallery@yahoo.com>
2022-01-07 08:39:06 -0500

    Modifications to the BuildTableLayout class was made to better manage the layout of TableLayouts for the Author and Files tables. The delete button method was duplicated and modified to being specific to each Author and File table needs. The result allowed the use of the layoutParams.weight to be separately managed and adjusted which allowed the float to be used for the Author table.

--- app/src/main/java/com/mistywillow/researchdb/BuildTableLayout.java
+++ app/src/main/java/com/mistywillow/researchdb/BuildTableLayout.java
@@ -22,7 +22,7 @@ public class BuildTableLayout extends AppCompatActivity{
             row.addView(addRowTextViewToTable(context, fileName, true));
         }
         if (!bold) {
-            row.addView(setupDeleteRowButton(context, table, fileName));
+            row.addView(setupFileDeleteRowButton(context, table, fileName));
             for(int r=1; r < 2; r++){
                 row.setTag(fileName);
                 row.addView(addRowTextViewToTable(context, fileName, false));
@@ -39,13 +39,51 @@ public class BuildTableLayout extends AppCompatActivity{
         trLayoutParams.weight = 1;
         btnAddRow.setLayoutParams(trLayoutParams);
         btnAddRow.setBackgroundColor(Color.WHITE);
-        btnAddRow.setText(R.string.lbl_column_file_id);
+        btnAddRow.setText("Delete");
         btnAddRow.setTypeface(Typeface.DEFAULT,Typeface.BOLD);
         btnAddRow.setTextSize(12);
         btnAddRow.setGravity(Gravity.CENTER);
         btnAddRow.setPadding(5,5,5,5);
         //btnAddRow.setOnClickListener(v -> table.addView(setupFilesTableRow(context, table, "", "", false)));
         return btnAddRow;
+    }
+
+    public static Button setupFileDeleteRowButton(Context context, TableLayout table, String tag){
+        Button btnDelete = new Button(context);
+        TableRow.LayoutParams trLayoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT);
+        trLayoutParams.setMargins(3,3,3,3);
+        trLayoutParams.weight = 1;
+        trLayoutParams.height =75;
+        btnDelete.setLayoutParams(trLayoutParams);
+        btnDelete.setBackgroundColor(Color.WHITE);
+        btnDelete.setText("-");
+        btnDelete.setTypeface(Typeface.DEFAULT,Typeface.BOLD);
+        btnDelete.setTextSize(14);
+        btnDelete.setGravity(Gravity.CENTER);
+        btnDelete.setPadding(5,5,5,5);
+        btnDelete.setTag(tag);
+        btnDelete.setOnClickListener(v -> {
+            String btnTag = btnDelete.getTag().toString();
+            deleteTableRows(table, btnTag);
+        });
+        return btnDelete;
+    }
+
+    public static TextView addRowTextViewToTable(Context context, String value, boolean bold){
+        TextView tv;
+        tv = new TextView(context);
+        TableRow.LayoutParams trLayoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT);
+        trLayoutParams.setMargins(3,3,3,3);
+        trLayoutParams.weight = 5;
+        tv.setLayoutParams(trLayoutParams);
+        tv.setText(String.valueOf(value));
+        if(bold) tv.setTypeface(null, Typeface.BOLD);
+        tv.setTextSize(12);
+        tv.setGravity(Gravity.CENTER);
+        tv.setPadding(8,8,8,8);
+        tv.setBackgroundColor(Color.WHITE);
+
+        return tv;
     }
 
     // AUTHORS ======================================================
@@ -55,7 +93,7 @@ public class BuildTableLayout extends AppCompatActivity{
         TableRow row = new TableRow(context);
         if(bool_AddNames) {
             if (bool_DeleteBtn) {
-                row.addView(setupDeleteRowButton(context, table, ""));
+                row.addView(setupAuthorDeleteRowButton(context, table, ""));
                 Log.e("BuildTableLayout", "setupDeleteRowButton");
                 row.addView(addEditTextToTable(context, first));
                 row.setClickable(true);
@@ -79,7 +117,7 @@ public class BuildTableLayout extends AppCompatActivity{
         }
         if(!bool_AddNames) {
             String tag = "";
-            row.addView(setupDeleteRowButton(context, table, tag));
+            row.addView(setupFileDeleteRowButton(context, table, tag));
             for(int r=1; r<5;r++) {
                 row.addView(addEditTextToTable(context, ""));
                 row.setClickable(true);
@@ -104,13 +142,11 @@ public class BuildTableLayout extends AppCompatActivity{
         return btnAddRow;
     }
 
-    // ADDITIONAL METHODS
-
-    public static Button setupDeleteRowButton(Context context, TableLayout table, String tag){
+    public static Button setupAuthorDeleteRowButton(Context context, TableLayout table, String tag){
         Button btnDelete = new Button(context);
         TableRow.LayoutParams trLayoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT);
         trLayoutParams.setMargins(3,3,3,3);
-        trLayoutParams.weight = 1;
+        trLayoutParams.weight = 0.05f;
         trLayoutParams.height =75;
         btnDelete.setLayoutParams(trLayoutParams);
         btnDelete.setBackgroundColor(Color.WHITE);
@@ -131,7 +167,8 @@ public class BuildTableLayout extends AppCompatActivity{
         EditText editText = new EditText(context);
         TableRow.LayoutParams trLayoutParams = new TableRow.LayoutParams();
         trLayoutParams.setMargins(3,3,3,3);
-        trLayoutParams.weight = 1;
+        trLayoutParams.weight = 0.2375f;
+        trLayoutParams.width = 0;
         editText.setLayoutParams(trLayoutParams);
         editText.setBackgroundColor(Color.WHITE);
         editText.setText(value);
@@ -142,22 +179,7 @@ public class BuildTableLayout extends AppCompatActivity{
         return editText;
     }
 
-    public static TextView addRowTextViewToTable(Context context, String value, boolean bold){
-        TextView tv;
-        tv = new TextView(context);
-        TableRow.LayoutParams trLayoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT);
-        trLayoutParams.setMargins(3,3,3,3);
-        trLayoutParams.weight = 5;
-        tv.setLayoutParams(trLayoutParams);
-        tv.setText(String.valueOf(value));
-        if(bold) tv.setTypeface(null, Typeface.BOLD);
-        tv.setTextSize(12);
-        tv.setGravity(Gravity.CENTER);
-        tv.setPadding(8,8,8,8);
-        tv.setBackgroundColor(Color.WHITE);
-
-        return tv;
-    }
+    // ADDITIONAL METHODS
 
     public static void deleteTableRows(TableLayout table, String tag){
         for (int i=1; i < table.getChildCount(); i++){
